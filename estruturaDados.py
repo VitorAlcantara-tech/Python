5e144436f77ae98867df99de55287322c4cbb0b1
Vitor <vitoralcan5@gmail.com>
2025-06-08 23:40:09 -0300

    comentado

--- estruturaDados.py
+++ estruturaDados.py
@@ -16,30 +16,30 @@
 # print("Números ordenados:", numeros)
 
 # FUNCAO CALCULADORA
-n1 = int(input("Digite o primeiro número: "))
-n2 = int(input("Digite o segundo número: "))
-operacao = input("Digite a operação da conta: ")
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))
+# operacao = input("Digite a operação da conta: ")
 
-if operacao not in ['+', '-', '*', '/']:
-    print("Digite uma operação válida.")
+# if operacao not in ['+', '-', '*', '/']:
+#     print("Digite uma operação válida.")
 
-elif operacao == '+':
-    soma = n1 + n2
-    print(f"A soma dos números {n1} e {n2} é igual {soma}.")
+# elif operacao == '+':
+#     soma = n1 + n2
+#     print(f"A soma dos números {n1} e {n2} é igual {soma}.")
 
-elif operacao == '-':
-    subtracao = n1 - n2
-    print(f"A subtração dos números {n1} e {n2} é igual {subtracao}.")
+# elif operacao == '-':
+#     subtracao = n1 - n2
+#     print(f"A subtração dos números {n1} e {n2} é igual {subtracao}.")
 
-elif operacao == '*':
-    multiplicacao = n1 * n2
-    print(f"A multiplicação dos números {n1} e {n2} é igual {multiplicacao}.")
+# elif operacao == '*':
+#     multiplicacao = n1 * n2
+#     print(f"A multiplicação dos números {n1} e {n2} é igual {multiplicacao}.")
 
-elif operacao == '/':
-    if n2 == 0:
-        print("Erro: divisão por zero não é permitida.")
-    else:
-        divisao = n1 / n2
-        print(f"A divisão dos números {n1} e {n2} é igual {divisao}.")
+# elif operacao == '/':
+#     if n2 == 0:
+#         print("Erro: divisão por zero não é permitida.")
+#     else:
+#         divisao = n1 / n2
+#         print(f"A divisão dos números {n1} e {n2} é igual {divisao}.")
 
 

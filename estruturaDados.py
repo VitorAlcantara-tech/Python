7612199cbb2b275faeba9d353e49117cd7b81126
Vitor <vitoralcan5@gmail.com>
2025-06-08 23:39:16 -0300

    update funcao calculadora

--- estruturaDados.py
+++ estruturaDados.py
@@ -1,19 +1,45 @@
 # ORDENACAO MANUAL
-# Solicita 5 números ao usuário
-numeros = []
+# numeros = []
 
-for i in range(5):
-    num = int(input(f"Digite o {i+1}º número: "))
-    numeros.append(num)
+# for i in range(5):
+#     num = int(input(f"Digite o {i+1}º número: ")) # Solicita 5 números ao usuário
+#     numeros.append(num)
 
-# Ordenação manual - Método da Bolha (Bubble Sort)
-n = len(numeros)
+# # Ordenação manual - Método da Bolha (Bubble Sort)
+# n = len(numeros)
 
-for i in range(n):
-    for j in range(0, n - i - 1):
-        if numeros[j] > numeros[j + 1]:
-            # Troca os elementos
-            numeros[j], numeros[j + 1] = numeros[j + 1], numeros[j]
+# for i in range(n):
+#     for j in range(0, n - i - 1):
+#         if numeros[j] > numeros[j + 1]:
+#             numeros[j], numeros[j + 1] = numeros[j + 1], numeros[j] # Troca os elementos de lugar
 
-# Exibe os números ordenados
-print("Números ordenados:", numeros)
+# print("Números ordenados:", numeros)
+
+# FUNCAO CALCULADORA
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+operacao = input("Digite a operação da conta: ")
+
+if operacao not in ['+', '-', '*', '/']:
+    print("Digite uma operação válida.")
+
+elif operacao == '+':
+    soma = n1 + n2
+    print(f"A soma dos números {n1} e {n2} é igual {soma}.")
+
+elif operacao == '-':
+    subtracao = n1 - n2
+    print(f"A subtração dos números {n1} e {n2} é igual {subtracao}.")
+
+elif operacao == '*':
+    multiplicacao = n1 * n2
+    print(f"A multiplicação dos números {n1} e {n2} é igual {multiplicacao}.")
+
+elif operacao == '/':
+    if n2 == 0:
+        print("Erro: divisão por zero não é permitida.")
+    else:
+        divisao = n1 / n2
+        print(f"A divisão dos números {n1} e {n2} é igual {divisao}.")
+
+
